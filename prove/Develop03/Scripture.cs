585a279adc28ceeba73c072439bc5d5ce28ef6ab
josequemba <joseeliud33@gmail.com>
2023-09-26 20:48:24 +0100

    added classes in the project

---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Scripture
+{
+    private Reference _references;
+    private List<Word> _words = new List<Word>();
+
+    public Scripture (Reference Reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords (int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText ()
+    {
+        return "";
+    }
+
+    public bool IsCompletelyHidden ()
+    {
+        return true;
+    }
+}
