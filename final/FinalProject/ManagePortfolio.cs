0de8ff6c8fe28021f40984d4c2a2319396840c65
josequemba <joseeliud33@gmail.com>
2023-10-18 04:09:23 +0100

    small changes

--- final/FinalProject/ManagePortfolio.cs
+++ final/FinalProject/ManagePortfolio.cs
@@ -17,7 +17,25 @@ public class ManagePortfolio
 
     public void DisplayMenu ()
     {
+        List<string> menu = new List<string>
+        {
+            "",
+            "Enter Graduation Requirements",
+            "Enter classes",
+            "Display Portfolio",
+            "Report Grades",
+            "Report Completed/ Dropped",
+            "Save Portfolio",
+            "Load Portfolio",
+            "Quit"
+        };
 
+        Console.WriteLine("\nMenu Options:");
+        for (int i = 1; i < menu.Count; i++)
+        {
+            Console.WriteLine($"  {i}. {menu[i]}");
+        }
+        Console.Write("Select an option from your menu: ");
     }
 
     public void Save ()
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello FinalProject World!");
+        //Console.WriteLine("Hello FinalProject World!");
+        int userInput = 0;
+
+        ManagePortfolio manageportfolio = new ManagePortfolio ();
+ 
+
+        while (userInput != 7)
+        {
+            Console.Clear ();
+            manageportfolio.DisplayMenu ();
+            Console.Write ("");
+            userInput = int.Parse(Console.ReadLine ());
+        }
     }
 }
